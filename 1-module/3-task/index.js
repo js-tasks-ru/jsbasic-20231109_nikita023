d4fa335f2734871d7d76c0bd8d4c9db6e2777858
23sns <123@123.ru>
2023-11-17 22:07:05 +0300

    Solution of 1-module 3-task

--- 1-module/3-task/index.js
+++ 1-module/3-task/index.js
@@ -1,3 +1,11 @@
 function ucFirst(str) {
-  // ваш код...
+  if (str === '') {
+    return '';
+  }
+
+  if (str.length === 1) {
+    return str.toUpperCase();
+  }
+
+  return str[0].toUpperCase() + str.slice(1);
 }
