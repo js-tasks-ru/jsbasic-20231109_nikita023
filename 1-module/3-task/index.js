8ad50780cd1e7f8416881bbe4fd7fb82bdf3370f
23sns <123@123.ru>
2023-11-18 18:20:29 +0300

    Fix review comments for 1-module 3-task 4-task

--- 1-module/3-task/index.js
+++ 1-module/3-task/index.js
@@ -1,11 +1,7 @@
 function ucFirst(str) {
-  if (str === '') {
-    return '';
-  }
-
-  if (str.length === 1) {
+  if (str.length <= 1) {
     return str.toUpperCase();
   }
-
+ 
   return str[0].toUpperCase() + str.slice(1);
 }
