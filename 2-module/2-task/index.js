c65ab3a7e084bd375c0c73b858aecb6ddf8d7314
23sns <123@123.ru>
2023-11-23 18:05:30 +0300

    Fix review comments for 2-module 1-2-task

--- 2-module/2-task/index.js
+++ 2-module/2-task/index.js
@@ -2,9 +2,5 @@ function isEmpty(obj) {
   for (let key in obj) {
     return false;
   }
-    
-  if (obj.key === undefined || obj.key === '') {
-   return true;
-  }
-   return false;
+    return true;
 }
