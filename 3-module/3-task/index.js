45025edaf817db3f0adc960b93ae588e8185fbf2
23sns <123@123.ru>
2023-12-04 16:14:14 +0300

    Solution of 3-module 1-2-3-4-5-task

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,3 +1,20 @@
 function camelize(str) {
-  // ваш код...
+  let arr = str.split('');
+  let result = [];
+  let needUpperCase = false;
+  
+  arr.forEach((item, index, array) => {
+      if (item === '-') {
+          needUpperCase = true;
+          return;
+      };
+
+      if (needUpperCase) {
+          result.push(item.toUpperCase());
+          needUpperCase = false;
+      } else {
+          result.push(item.toLowerCase());
+      };
+  });
+  return result.join('');
 }
