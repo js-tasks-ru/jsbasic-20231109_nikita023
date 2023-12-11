e2677092abc9c7a4131bd372fddea01a0ff435e7
23sns <123@123.ru>
2023-12-11 17:57:04 +0300

    Solution of 4-module 1-2-3-task

--- 4-module/2-task/index.js
+++ 4-module/2-task/index.js
@@ -1,3 +1,10 @@
 function makeDiagonalRed(table) {
-  // ваш код...
+  for (let i = 0; i < table.rows.length; i++) {
+    for (let j = 0; j < table.rows[i].cells.length; j++) {
+      if (i === j) {
+        table.rows[i].cells[j].style.backgroundColor = 'red';
+        break;
+      }
+    }
+  }
 }
--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,24 @@
 function highlight(table) {
-  // ваш код...
+  for (let i = 0; i < table.rows.length; i++) {
+    const row = table.rows[i];
+    
+    const availableValue = row.cells[3].dataset['available'];
+    if (availableValue === 'true') {
+      row.classList.add('available');
+    } else if (availableValue === 'false') {
+      row.classList.add('unavailable');
+    } else if (availableValue === undefined) {
+      row.setAttribute('hidden', 'true');    
+    }
+
+    if (row.cells[2].innerText === 'm') {
+      row.classList.add('male');
+    } else if (row.cells[2].innerText === 'f') {
+      row.classList.add('female');
+    }
+
+    if (+row.cells[1].innerText < 18) {
+      row.style.textDecoration = 'line-through';
+    }
+  }
 }
